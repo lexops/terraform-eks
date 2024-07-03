a945c560281c6cb71c7cbe8e4c622c5413400e1d
lexops <134613820+lexops@users.noreply.github.com>
2024-07-03 12:18:13 -0300

    add priv subnets and variables

--- variables.tf
+++ variables.tf
@@ -2,3 +2,8 @@ variable "cidr_block" {
   type        = string
   description = "Networking CIDR block to be used in the vpc"
 }
+
+variable "project_name" {
+  type        = string
+  description = "Project name to be used to name the resources (Name tag)"
+}
