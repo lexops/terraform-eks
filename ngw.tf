6a9296f00d15be918f3b482fa16e976452146a1f
lexops <134613820+lexops@users.noreply.github.com>
2024-07-03 17:12:03 -0300

    feat: add priv subnets and natgws

---
+++ ngw.tf
@@ -0,0 +1,56 @@
+resource "aws_eip" "eks_ngw_eip_1a" {
+  domain = "vpc"
+  tags = merge(
+    local.tags,
+    {
+      Name = "${var.project_name}-eip-1a"
+    }
+  )
+}
+
+resource "aws_eip" "eks_ngw_eip_1b" {
+  domain = "vpc"
+  tags = merge(
+    local.tags,
+    {
+      Name = "${var.project_name}-eip-1b"
+    }
+  )
+}
+
+resource "aws_nat_gateway" "eks_ngw_1a" {
+  allocation_id = aws_eip.eks_ngw_eip_1a.id
+  subnet_id     = aws_subnet.eks_subnet_public_1a.id
+
+  tags = merge(
+    local.tags,
+    {
+      Name = "${var.project_name}-ngw-1a"
+    }
+  )
+
+}
+
+resource "aws_nat_gateway" "eks_ngw_1b" {
+  allocation_id = aws_eip.eks_ngw_eip_1b.id
+  subnet_id     = aws_subnet.eks_subnet_public_1b.id
+
+  tags = merge(
+    local.tags,
+    {
+      Name = "${var.project_name}-ngw-1b"
+    }
+  )
+
+}
+
+resource "aws_route_table_association" "eks_rtb_assoc_priv_1a" {
+  subnet_id      = aws_subnet.eks_subnet_private_1a.id
+  route_table_id = aws_route_table.eks_private_route_table_1a.id
+}
+
+resource "aws_route_table_association" "eks_rtb_assoc_priv_1b" {
+  subnet_id      = aws_subnet.eks_subnet_private_1b.id
+  route_table_id = aws_route_table.eks_private_route_table_1b.id
+}
+
