77f58f421d192eeb8188367cc5e6104a34e8918a
lexops <134613820+lexops@users.noreply.github.com>
2024-07-03 10:55:44 -0300

    decouple tags and add cidrsubnet function

---
+++ locals.tf
@@ -0,0 +1,8 @@
+locals {
+  tags = {
+    Department   = "DevOps"
+    Organization = "Infrastructure and Operations"
+    Project      = "EKS"
+    Environment  = "Development"
+  }
+}
---
+++ variables.tf
@@ -0,0 +1,4 @@
+variable "cidr_block" {
+  type        = string
+  description = "Networking CIDR block to be used in the vpc"
+}
